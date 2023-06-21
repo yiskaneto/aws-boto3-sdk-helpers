db788f68eb7e9a2ffee4fd9bb07158dfac93da62
escanoru <yitzoyi@outlook.com>
2023-06-21 16:45:40 +0000

    indentantion updated

--- cloudwatch/check_alarm_status.py
+++ cloudwatch/check_alarm_status.py
@@ -36,8 +36,9 @@ def cw_describe_alarms():
         )
         if response['MetricAlarms'][0]['StateValue'] != {args.alarm_status}:
             print(f'''
-            The the last status of the {response['MetricAlarms'][0]['AlarmName']} is not in {args.alarm_status} state
-                  ''')
+                  ['MetricAlarms'][0]['StateValue']
+                  The the last status of the {response['MetricAlarms'][0]['AlarmName']} is not in {args.alarm_status} state
+            ''')
 
     except botocore.exceptions.ClientError as error_found:
         if error_found.response['Error']['Code'] in exceptions:
