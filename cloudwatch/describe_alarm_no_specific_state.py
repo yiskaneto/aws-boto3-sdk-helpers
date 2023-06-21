5046afb78847047714ecde5a932176920b58634a
escanoru <yitzoyi@outlook.com>
2023-06-21 17:47:20 +0000

    state removed from parameters

--- cloudwatch/describe_alarm_no_specific_state.py
+++ cloudwatch/describe_alarm_no_specific_state.py
@@ -31,7 +31,6 @@ def cw_describe_alarms():
         response = client.describe_alarms(
             AlarmNames=[f"{args.alarm_name}"],
             AlarmTypes=['MetricAlarm'],
-            StateValue=f"{args.alarm_status}",
             MaxRecords=50,
         )
         print(response['MetricAlarms'])
