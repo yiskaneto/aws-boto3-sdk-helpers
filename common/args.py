0f0e13447a25b42b82a672dcf3cf9903c61448a8
yiskaneto <44711170+yiskaneto@users.noreply.github.com>
2024-06-15 17:05:42 -0600

    removing uneeded variables

--- common/args.py
+++ common/args.py
@@ -1,7 +1,9 @@
 import argparse
 
-##########################################################################################################
+AWS_REGION_CONST = "Target AWS region where to perform the action"
+
 ## Bucket
+###############################################################################
 def bucket_args():
     """
     Initializes the arguments needed to excecute operations on a given S3 bucket.
@@ -12,13 +14,29 @@ def bucket_args():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument("--bucket", required=True, help="Name of the bucket")
-    parser.add_argument("--aws_region", required=True, help="aws region where to look for the bucket")
+    parser.add_argument("--aws-region", required=True, help=AWS_REGION_CONST)
     parser.add_argument("--prefix", required=True, help="Buckets Prefix")
     args = parser.parse_args()
     return args
 
-bucket_args_call = bucket_args()
-##########################################################################################################
+###############################################################################
+
+## CloudWatch Logs
+###############################################################################
+def cloudwatch_logs_args():
+    """
+    Initializes the arguments needed to excecute operations on a given cloudwatch log group.
+
+    Example
+    -------
+    a_cloudwatch_log_group_function(cloudwatch_logs_args())
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--group-name", required=True, help="CloudWatch log group name")
+    parser.add_argument("--aws-region", required=True, help=AWS_REGION_CONST)
+    args = parser.parse_args()
+    return args
+#################################################################################
 
 if __name__ == "__main__":
-    print(bucket_args())
+    print("Test")
--- common/boto_client_declaration.py
+++ common/boto_client_declaration.py
@@ -2,7 +2,29 @@ import boto3
 
 def s3_client(args):
     """
+    Description
+    -----------
     Initialize the S3 bucket client for the Python AWS SDK.
     """
     s3 = boto3.client('s3', region_name=args.aws_region)
     return s3
+
+def cloudwatch_logs_client(args):
+    """
+    Description
+    -----------
+    Initialize the cloudwatch logs client for the Python AWS SDK.
+
+    Parameters
+    ------------
+        args: parser.parse_args()
+            The args object needed to initialize the client object.
+
+    Return
+    -----------
+        age : boto3.client()
+            Configured boto3 client.
+    """
+    logs = boto3.client('logs', region_name=args.aws_region)
+    return logs
+
