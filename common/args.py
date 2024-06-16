be54d038da9eaac1438fad30d0e680077bde352d
yiskaneto <44711170+yiskaneto@users.noreply.github.com>
2024-06-15 19:41:56 -0600

    Adding new files

--- common/args.py
+++ common/args.py
@@ -2,10 +2,22 @@ import argparse
 
 AWS_REGION_CONST = "Target AWS region where to perform the action"
 
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
 ## Bucket
 ###############################################################################
 def bucket_args():
     """
+    Description
+    -----------
     Initializes the arguments needed to excecute operations on a given S3 bucket.
 
     Example
@@ -25,6 +37,8 @@ def bucket_args():
 ###############################################################################
 def cloudwatch_logs_args():
     """
+    Description
+    -----------
     Initializes the arguments needed to excecute operations on a given cloudwatch log group.
 
     Example
@@ -33,6 +47,7 @@ def cloudwatch_logs_args():
     """
     parser = argparse.ArgumentParser()
     parser.add_argument("--group-name", required=True, help="CloudWatch log group name")
+    parser.add_argument("--dry-run", type=str2bool, required=True, help="Whether or not to to perfom a dry run, if set to True the log group will not be removed.")
     parser.add_argument("--aws-region", required=True, help=AWS_REGION_CONST)
     args = parser.parse_args()
     return args
